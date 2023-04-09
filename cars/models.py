58036dd3847c9040d73e4951934121fc95da0544
Asror Kabulov <ansoriy559@gmail.com>
2023-04-09 11:20:42 +0600

    добавил поля

--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,8 @@ class Cars(models.Model):
     gos_num = models.CharField(max_length=10, verbose_name="Гос номер")
     model = models.CharField(max_length=100, verbose_name="Модель")
     color = models.CharField(max_length=100, verbose_name="Цвет")
+    year = models.PositiveSmallIntegerField(verbose_name="Год выпуска")
+    rudder_location = models.CharField(verbose_name="Расположение руля", max_length=100)
 
     def __str__(self):
         return self.gos_num
