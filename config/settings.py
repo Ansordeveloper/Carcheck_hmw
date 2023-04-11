fe99a3cde7b5be9c19c144177253d4015ef9bedf
Asror Kabulov <ansoriy559@gmail.com>
2023-04-11 06:18:01 +0600

    *

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# STATIC_ROOT = '/var/www/Ansor/static'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
